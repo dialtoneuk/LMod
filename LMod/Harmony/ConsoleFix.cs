0d1dd746aa235e77a5f8ab24a9fd5464a603ecb4
Llydia Cross <llydiacross@gmaioil.com>
2021-03-05 05:57:18 +0000

    Add project files.

---
+++ LMod/Harmony/ConsoleFix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+[HarmonyPatch(typeof(GUIWindowConsole))]
+[HarmonyPatch("openConsole")]
+class LMod_ConsoleFix
+{
+
+    /**
+     * Really simple fix to stop exceptions automatically opening console
+     **/
+    static bool Prefix()
+    {
+
+        return false;
+    }
+}
+
---
+++ LMod/Harmony/HarmonyInit.cs
@@ -0,0 +1,26 @@
+﻿
+using DMT;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+public class LMod_Init : IHarmony
+{
+
+    /**
+     * This essentially is where it all begins, and starts the process that patches in our edits
+     **/
+
+    public void Start()
+    {
+        // Reduce extra logging stuff
+        Application.SetStackTraceLogType(UnityEngine.LogType.Log, StackTraceLogType.None);
+        Application.SetStackTraceLogType(UnityEngine.LogType.Warning, StackTraceLogType.None);
+
+        var harmony = new Harmony("com.llydia.lmod");
+        Harmony.DEBUG = true;
+
+        //automatically patches everything
+        harmony.PatchAll();
+    }
+}
